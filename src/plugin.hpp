ce2fe6af9cd5f6802e7faa88ea48451b0369b9e6
Jörg Bauder <34174944+jb0000@users.noreply.github.com>
2021-03-28 16:18:04 +0200

    big cleanup and first style

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -6,23 +6,4 @@ using namespace rack;
 
 extern Plugin* pluginInstance;
 
-extern Model* modelBraids;
 extern Model* modelPlaits;
-extern Model* modelElements;
-extern Model* modelTides;
-extern Model* modelTides2;
-extern Model* modelClouds;
-extern Model* modelWarps;
-extern Model* modelRings;
-extern Model* modelLinks;
-extern Model* modelKinks;
-extern Model* modelShades;
-extern Model* modelBranches;
-extern Model* modelBlinds;
-extern Model* modelVeils;
-extern Model* modelFrames;
-extern Model* modelStages;
-extern Model* modelMarbles;
-extern Model* modelRipples;
-extern Model* modelShelves;
-extern Model* modelStreams;
